29c0bfeb4d1b7bf3eb11f6ea3301d29b8421c330
Arnauld <arnauld.loyer@gmail.com>
2019-02-05 23:40:54 +0100

    add word count

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,5 +6,15 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+  	sentence
+  		|> String.downcase()
+  		|> String.split(~r"[\s$!@#%^&:;,._]+")
+  		|> Enum.reduce(%{}, &Words.add_or_increment/2)
+  end
+
+  def add_or_increment("", map), do: map
+  def add_or_increment(word, map) do
+  	nb = Map.get(map, word, 0)
+  	Map.put(map, word, nb + 1)
   end
 end
