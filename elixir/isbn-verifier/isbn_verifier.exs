5d35616c00c9f7f022e45fcb667c76ec69daa1aa
Arnauld <arnauld.loyer@gmail.com>
2019-02-09 23:41:23 +0100

    add isbn verifier

---
+++ elixir/isbn-verifier/isbn_verifier.exs
@@ -0,0 +1,33 @@
+defmodule ISBNVerifier do
+  @doc """
+    Checks if a string is a valid ISBN-10 identifier
+
+    ## Examples
+
+      iex> ISBNVerifier.isbn?("3-598-21507-X")
+      true
+
+      iex> ISBNVerifier.isbn?("3-598-2K507-0")
+      false
+
+  """
+  @spec isbn?(String.t()) :: boolean
+  def isbn?(isbn) do
+    case Regex.match?(~r/^\d\-?\d{3}\-?\d{5}\-?(\d|X)$/, isbn) do
+      false -> false
+      true ->
+        isbn 
+          |> String.replace("-", "")
+          |> String.to_charlist() 
+          |> Enum.zip(10..1)
+          |> Enum.map(&parse_and_scale/1)
+          |> Enum.sum()
+          |> rem(11) == 0
+    end
+  end
+
+  defp parse_and_scale({?X, scale}), do: 10 * scale
+  defp parse_and_scale({digit, scale}) when digit >= 48 and digit <= 57 do
+    (digit - 48) * scale
+  end
+end
