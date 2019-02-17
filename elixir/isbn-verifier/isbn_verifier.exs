aa08965f896a93359c197a2243de5dbc3cd45503
Arnauld <arnauld.loyer@gmail.com>
2019-02-17 18:44:58 +0100

    reformat

--- elixir/isbn-verifier/isbn_verifier.exs
+++ elixir/isbn-verifier/isbn_verifier.exs
@@ -14,19 +14,22 @@ defmodule ISBNVerifier do
   @spec isbn?(String.t()) :: boolean
   def isbn?(isbn) do
     case Regex.match?(~r/^\d\-?\d{3}\-?\d{5}\-?(\d|X)$/, isbn) do
-      false -> false
+      false ->
+        false
+
       true ->
-        isbn 
-          |> String.replace("-", "")
-          |> String.to_charlist() 
-          |> Enum.zip(10..1)
-          |> Enum.map(&parse_and_scale/1)
-          |> Enum.sum()
-          |> rem(11) == 0
+        isbn
+        |> String.replace("-", "")
+        |> String.to_charlist()
+        |> Enum.zip(10..1)
+        |> Enum.map(&parse_and_scale/1)
+        |> Enum.sum()
+        |> rem(11) == 0
     end
   end
 
   defp parse_and_scale({?X, scale}), do: 10 * scale
+
   defp parse_and_scale({digit, scale}) when digit >= 48 and digit <= 57 do
     (digit - 48) * scale
   end
