31dd72f865e73207543f3cabdd24cd7795feeab1
Arnauld <arnauld.loyer@gmail.com>
2019-03-07 09:27:21 +0100

    less self explanatory but more efficient

--- elixir/clock/clock.exs
+++ elixir/clock/clock.exs
@@ -18,16 +18,24 @@ defmodule Clock do
   """
   @spec new(integer, integer) :: Clock
   def new(hour, minute) do
-    {delta_hour, delta_minute} = positive_minute({hour,minute})
+    {delta_hour, delta_minute} = positive_minute({hour, minute})
     effective_hour = positive_hour(delta_hour + div(delta_minute, 60))
     %Clock{hour: rem(effective_hour, 24), minute: rem(delta_minute, 60)}
   end
 
-  defp positive_hour(hour) when hour >=0, do: hour
-  defp positive_hour(hour), do: positive_hour(hour + 24)
+  defp positive_hour(hour) when hour >= 0, do: hour
 
-  defp positive_minute({hour,minute}) when minute >= 0, do: {hour, minute}
-  defp positive_minute({hour,minute}), do: positive_minute({hour - 1, minute + 60})
+  defp positive_hour(hour) do
+    num = div(-hour, 24) + 1
+    hour + 24 * num
+  end
+
+  defp positive_minute({hour, minute}) when minute >= 0, do: {hour, minute}
+
+  defp positive_minute({hour, minute}) do
+    num = div(-minute, 60) + 1
+    {hour - num, minute + 60 * num}
+  end
 
   @doc """
   Adds two clock times:
