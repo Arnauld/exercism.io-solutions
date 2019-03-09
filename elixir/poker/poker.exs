1e56ce9ebc82d5389f1b45a3b991d38fdb6d3011
Arnauld <arnauld.loyer@gmail.com>
2019-03-09 01:19:50 +0100

    finish poker

--- elixir/poker/poker.exs
+++ elixir/poker/poker.exs
@@ -93,47 +93,127 @@ defmodule Poker do
   end
 
   defp compare(cards1, cards2) do
-    group1 = group_by_num(cards1)
-    group2 = group_by_num(cards2)
+    group1 = cards1 |> group_by_num()
+    group2 = cards2 |> group_by_num()
+    sorted1 = cards1 |> sort_cards_by_num()
+    sorted2 = cards2 |> sort_cards_by_num()
 
-    compare_straight(cards1, cards2)
+    compare_straight_flush(sorted1, sorted2)
+    |> otherwise(compare_four_of_a_kind(group1, group2))
+    |> otherwise(compare_fullhouse(group1, group2))
+    |> otherwise(compare_straight(sorted1, sorted2))
     |> otherwise(compare_set(group1, group2))
     |> otherwise(compare_pairs(group1, group2))
     |> otherwise(raw_score(cards1) - raw_score(cards2))
   end
 
-  defp compare_straight(cards1, cards2) do
-    straight1 =
-      cards1
-      |> sort_cards_by_num()
-      |> straight?()
+  defp compare_straight_flush(sorted1, sorted2) do
+    straight1? = sorted1 |> straight?()
+    straight2? = sorted2 |> straight?()
+    flush1? = sorted1 |> flush?()
+    flush2? = sorted2 |> flush?()
 
-    straight2 =
-      cards2
-      |> sort_cards_by_num()
-      |> straight?()
-
-    IO.puts("\n ::straight ... #{inspect(cards1 |> sort_cards_by_num())}: #{straight1}")
-    IO.puts("\n ::straight ... #{inspect(cards2 |> sort_cards_by_num())}: #{straight2}")
+    straight_flush1? = straight1? and flush1?
+    straight_flush2? = straight2? and flush2?
 
     cond do
-      straight1 and straight2 ->
-        score_nums(cards1 |> Enum.map(&num_of_card/1)) -
-          score_nums(cards2 |> Enum.map(&num_of_card/1))
+      straight_flush1? and straight_flush2? ->
+        0
 
-      straight1 ->
-        -1
-
-      straight2 ->
+      straight_flush1? ->
         +1
+
+      straight_flush2? ->
+        -1
 
       true ->
         0
     end
   end
 
+  defp compare_four_of_a_kind(group1, group2) do
+    four_of_a_kind1 = group1 |> Map.get(4, [])
+    four_of_a_kind2 = group2 |> Map.get(4, [])
+    four_of_a_kind1? = four_of_a_kind1 |> length() > 0
+    four_of_a_kind2? = four_of_a_kind2 |> length() > 0
+
+    cond do
+      four_of_a_kind1? and four_of_a_kind2? ->
+        [v1] = four_of_a_kind1
+        [v2] = four_of_a_kind2
+        v1 - v2
+
+      four_of_a_kind1? ->
+        +1
+
+      four_of_a_kind2? ->
+        -1
+
+      true ->
+        0
+    end
+  end
+
+  defp compare_fullhouse(group1, group2) do
+    pairs1? = group1 |> Map.get(2, []) |> length() > 0
+    pairs2? = group2 |> Map.get(2, []) |> length() > 0
+    three_of_a_kind1? = group1 |> Map.get(3, []) |> length() > 0
+    three_of_a_kind2? = group2 |> Map.get(3, []) |> length() > 0
+
+    fullhouse1? = pairs1? and three_of_a_kind1?
+    fullhouse2? = pairs2? and three_of_a_kind2?
+
+    cond do
+      fullhouse1? and fullhouse2? ->
+        0
+
+      fullhouse1? ->
+        +1
+
+      fullhouse2? ->
+        -1
+
+      true ->
+        0
+    end
+  end
+
+  defp compare_straight(sorted1, sorted2) do
+    straight1 = sorted1 |> straight?()
+    straight2 = sorted2 |> straight?()
+    flush1 = sorted1 |> flush?()
+    flush2 = sorted2 |> flush?()
+
+    cond do
+      (flush1 and flush2) or (!flush1 and !flush2) ->
+        cond do
+          straight1 and straight2 ->
+            score_nums(sorted1 |> Enum.map(&num_of_card/1) |> reevaluate_ace_on_straight()) -
+              score_nums(sorted2 |> Enum.map(&num_of_card/1) |> reevaluate_ace_on_straight())
+
+          straight1 ->
+            +1
+
+          straight2 ->
+            -1
+
+          true ->
+            0
+        end
+
+      flush1 ->
+        +1
+
+      flush2 ->
+        -1
+    end
+  end
+
+  defp reevaluate_ace_on_straight([2, 3, 4, 5, @ace]), do: [1, 2, 3, 4, 5]
+  defp reevaluate_ace_on_straight(card_nums), do: card_nums
+
   defp sort_cards_by_num(cards) do
-    cards |> Enum.sort(fn {n1, _}, {n2, c} -> n1 >= n2 end)
+    cards |> Enum.sort(fn {n1, _}, {n2, c} -> n1 <= n2 end)
   end
 
   defp num_of_card({n, _}), do: n
@@ -152,15 +232,6 @@ defmodule Poker do
 
     (length(pairs1) - length(pairs2))
     |> otherwise(score_nums(pairs1) - score_nums(pairs2))
-  end
-
-  defp s() do
-    sorted_cards =
-      cards
-      |> Enum.sort(fn {n1, _}, {n2, c} -> n1 >= n2 end)
-
-    straight = straight?(sorted_cards)
-    flush = flush?(cards)
   end
 
   defp group_by_num(cards) do
@@ -200,6 +271,6 @@ defmodule Poker do
        when np1 == n + 1 and np2 == n + 2 and np3 == n + 3 and np4 == n + 4,
        do: true
 
-  defp straight?([{@ace, _}, {1, _}, {2, _}, {3, _}, {4, _}]), do: true
+  defp straight?([{2, _}, {3, _}, {4, _}, {5, _}, {@ace, _}]), do: true
   defp straight?(_), do: false
 end
